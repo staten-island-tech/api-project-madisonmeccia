918588ea23b835b81e580654caa9429d36ec8113
madisonmeccia <madisonnicole317@gmail.com>
2023-01-12 14:01:33 -0500

    h

---
+++ vite-project/main.js
@@ -0,0 +1,20 @@
+import "./style.css";
+var url = "http://colormind.io/api/";
+var data = {
+  model: "default",
+  input: [[44, 43, 44], [90, 83, 82], "N", "N", "N"],
+};
+
+var http = new XMLHttpRequest(); //delete and use fetch instead 
+
+http.onreadystatechange = function () {
+  if (http.readyState == 4 && http.status == 200) {
+    var palette = JSON.parse(http.responseText).result;
+  }
+};
+
+http.open("POST", url, true);
+http.send(JSON.stringify(data));
+
+// [[42, 41, 48], [90, 83, 84], [191, 157, 175], [188, 138, 125], [215, 170, 66]]
+// note that the input colors have changed as well, by a small amount
