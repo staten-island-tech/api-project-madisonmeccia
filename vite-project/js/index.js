49404532791544783d57528f185897283ec40df6
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2023-01-31 22:13:17 -0500

    n

--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -39,8 +39,19 @@ async function getData(URL) {
     console.log(error);
   }
 }
+
 DOMSelectors.btn.addEventListener("click", function () {
   getPicture();
 });
 
 getData(URL);
+
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
